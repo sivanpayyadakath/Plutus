9fe904c92882dae312b88e19f95f5c8ffac890ee
sivanpayyadakath <sivan.payyadakath@gmail.com>
2019-04-09 11:53:09 +0900

    basic structure

---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const ProjectSumary = () => {
+    return(
+        <div>
+            <div className="card">
+                <span className="card-title"></span>
+                <p>Project title</p>
+                <p>Posted by Sivan</p>
+                <p>5th June,2019</p>
+            </div>
+        </div>
+    )
+}
+
+export default ProjectSumary
