11049c79917690c5fa04fea93c9adc5525ca0efe
sivanpayyadakath <sivan.payyadakath@gmail.com>
2019-04-16 11:24:13 +0900

    adding dummy data

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -6,10 +6,11 @@ import { connect } from 'react-redux'
 
 class Dashboard extends Component{
     render(){
+        const { projects } = this.props;
         return(
             <div className="dashboard container">
                 <div className="row">
-                    <div className="col s12 m6"><ProjectList /></div>
+                    <div className="col s12 m6"><ProjectList projects = {projects}/></div>
                     <div className="col s12 m5 offset-m1"><Notifications/></div>
                 </div>
             </div>
--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
-const ProjectSumary = () => {
+const ProjectSumary = ({project}) => {
     return(
         <div>
             <div className="card">
                 <span className="card-title"></span>
-                <p>Project title</p>
-                <p>Posted by Sivan</p>
+                <p>{project.title}</p>
+                <p>{project.content}</p>
                 <p>5th June,2019</p>
             </div>
         </div>
